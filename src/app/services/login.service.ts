118a48c0067f3b3a4b49a8791652c48e9e04835d
Caique Silva <caiquesilva12@hotmail.com>
2024-12-29 12:43:31 -0300

    Fix
    New corrections in environmets

--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { tap } from 'rxjs';
-import { environment } from '../../../environment';
+import { environment } from '../../environments/environment';
 
 type LoginResponse = {
   token: string;
